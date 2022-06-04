03ecdcc2210875d880b01890c4834869bd5734d7
Jetom88 <shinhro3o@gmail.com>
2022-06-04 16:11:59 +0900

    🛠 Fix: Fix nav bar function

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,30 +1,30 @@
-import React from "react";
-import Layout from "../components/common/Layout";
+import React from 'react';
+import Layout from '../components/common/Layout';
 import NavBar from '../components/common/NavBar';
 import SearchBox from '../components/SearchBox';
-import ToiletInfo from "../components/ToiletInfo";
-import styles from "../styles/pages/list.module.scss";
+import ToiletInfo from '../components/ToiletInfo';
+import styles from '../styles/pages/list.module.scss';
 
 const arr = Array(5).fill(0);
 
 const List = () => {
-    return (
-        <Layout>
-            <section className={styles.list}>
-                <SearchBox />
-                <article className={styles.toiletInfoList}>
-                {arr.map(() => {
-                    return (
-                        <div className={styles.onList}>
-                            <ToiletInfo />
-                        </div>
-                    );
-                })}
-                </article>
-                <NavBar />
-            </section>
-        </Layout>
-    );
-}
+	return (
+		<Layout>
+			<section className={styles.list}>
+				<SearchBox />
+				<article className={styles.toiletInfoList}>
+					{arr.map(() => {
+						return (
+							<div className={styles.onList}>
+								<ToiletInfo />
+							</div>
+						);
+					})}
+				</article>
+				<NavBar num={[1, 0, 1, 1]} />
+			</section>
+		</Layout>
+	);
+};
 
 export default List;
