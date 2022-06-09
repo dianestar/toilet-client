7acff8adf46cc1c24c2bb409a2aeca967f36bb38
dianestar <dianestar@naver.com>
2022-06-10 01:49:12 +0900

    🛠Fix: Add hamburger icon

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from '../styles/components/searchBox.module.scss';
+import { ReactComponent as Hamburger } from "../assets/icons/hamburger.svg";
 import { ReactComponent as Search } from '../assets/icons/search.svg';
 
 const SearchBox = () => {
 	return (
 		<div className={styles.wrapper}>
+			<Hamburger className={styles.icon} />
 			<span className={styles.position}>서울시 어쩌구 2-16</span>
-			<Search />
+			<Search className={styles.icon}/>
 		</div>
 	);
 };
