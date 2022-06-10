d8183b89680e5ff5c93b572df666c8793714cff3
Jetom88 <shinhro3o@gmail.com>
2022-06-10 20:51:33 +0900

    💄Design: Modify nav css

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -2,30 +2,45 @@ import styles from '../../styles/components/navBar.module.scss';
 import { ReactComponent as Map } from '../../assets/icons/map.svg';
 import { ReactComponent as List } from '../../assets/icons/list.svg';
 import { ReactComponent as AddToilet } from '../../assets/icons/addToilet.svg';
-import { ReactComponent as Profile } from '../../assets/icons/profile.svg';
 import { ReactComponent as MapFill } from '../../assets/icons/mapFill.svg';
 import { ReactComponent as ListFill } from '../../assets/icons/listFill.svg';
 import { ReactComponent as AddToiletFill } from '../../assets/icons/addToiletFill.svg';
-import { ReactComponent as ProfileFill } from '../../assets/icons/profileFill.svg';
-import { useState } from 'react';
+import { ReactComponent as Close } from '../../assets/icons/close.svg';
 import BottomTabIcon from './BottomTabIcon';
 
 const NavBar = ({ num }) => {
 	return (
-		<ul className={styles.nav}>
-			<BottomTabIcon url={'map'} text={'지도'}>
-				{num[0] === 0 ? <MapFill /> : <Map />}
-			</BottomTabIcon>
-			<BottomTabIcon url={'list'} text={'목록'}>
-				{num[1] === 0 ? <ListFill /> : <List />}
-			</BottomTabIcon>
-			<BottomTabIcon url={'addToilet'} text={'화장실 추가'}>
-				{num[2] === 0 ? <AddToiletFill /> : <AddToilet />}
-			</BottomTabIcon>
-			<BottomTabIcon url={'profile'} text={'내 프로필'}>
-				{num[3] === 0 ? <ProfileFill /> : <Profile />}
-			</BottomTabIcon>
-		</ul>
+		<section className={styles.navOutSide}>
+			<article className={styles.nav}>
+				<div className={styles.profile}>
+					<Close className={styles.close} />
+					<img src="/images/KakaoTalk_Photo_2022-04-18-22-19-10 003.jpeg" />
+					<h2>jetom</h2>
+					<p>jetom@jetom.com</p>
+				</div>
+				<ul className={styles.iconList}>
+					<BottomTabIcon url={'map'} text={'지도'}>
+						{num[0] === 0 ? <MapFill /> : <Map />}
+					</BottomTabIcon>
+
+					<BottomTabIcon url={'list'} text={'목록'}>
+						{num[1] === 0 ? <ListFill /> : <List />}
+					</BottomTabIcon>
+
+					<BottomTabIcon url={'addToilet'} text={'화장실 추가'}>
+						{num[2] === 0 ? <AddToiletFill /> : <AddToilet />}
+					</BottomTabIcon>
+				</ul>
+				<div
+					className={styles.logout}
+					onClick={() => {
+						console.log('test');
+					}}
+				>
+					<p>로그아웃</p>
+				</div>
+			</article>
+		</section>
 	);
 };
 
