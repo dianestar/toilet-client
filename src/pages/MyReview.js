73194b0138d6cc6bd2fe372ad436a468d14e717c
dianestar <dianestar@naver.com>
2022-06-12 00:42:17 +0900

    🛠Fix: Change DeleteConfirm default state & few css codes

--- src/pages/MyReview.js
+++ src/pages/MyReview.js
@@ -8,11 +8,11 @@ import styles from "../styles/pages/myReview.module.scss";
 const arr = [0, 1, 0, 1, 0];
 
 const MyReview = () => {
-    const [open, setOpen] = useState(true);
+    const [open, setOpen] = useState(false);
 
     return (
         <Layout>
-            <section>
+            <section className={styles[`my-review`]}>
                 <Header text="리뷰 관리" />
                 <article className={styles.wrapper}>
                     {arr.map((value, index) => {
