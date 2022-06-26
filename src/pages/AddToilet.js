1b862da10fa8d45235043c400a0a2e678f5200d9
Jetom88 <shinhro3o@gmail.com>
2022-06-26 19:37:07 +0900

    💄Design: Add address css

--- src/pages/AddToilet.js
+++ src/pages/AddToilet.js
@@ -6,6 +6,7 @@ import AskReview from '../components/modal/AskReview';
 import styles from '../styles/pages/addToilet.module.scss';
 import BlueBtn from '../components/common/BlueBtn';
 import pinSelectedNoBg from '../assets/icons/pinSelectedNoBg.svg';
+import classNames from 'classnames';
 
 const AddToilet = () => {
 	const [open, setOpen] = useState(true);
@@ -19,30 +20,30 @@ const AddToilet = () => {
 
 		const map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
+		const imageSrc = pinSelectedNoBg, // 마커이미지의 주소입니다
+			imageSize = new kakao.maps.Size(22, 32), // 마커이미지의 크기입니다
+			imageOption = { offset: new kakao.maps.Point(22, 32) };
+
+		const markerImage = new kakao.maps.MarkerImage(
+			imageSrc,
+			imageSize,
+			imageOption,
+		);
+
 		let marker = new kakao.maps.Marker({
 			// 지도 중심좌표에 마커를 생성합니다
 			position: map.getCenter(),
+			image: markerImage,
 		});
 		// 지도에 마커를 표시합니다
-		marker.setMap(map);
-
-		// 지도에 클릭 이벤트를 등록합니다
-		// 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
-		kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
-			// 클릭한 위도, 경도 정보를 가져옵니다
-			var latlng = mouseEvent.latLng;
-
-			// 마커 위치를 클릭한 위치로 옮깁니다
-			marker.setPosition(latlng);
-		});
 
 		if (navigator.geolocation) {
 			// GeoLocation을 이용해서 접속 위치를 얻어옵니다
 			navigator.geolocation.getCurrentPosition(function (position) {
-				var lat = position.coords.latitude, // 위도
+				let lat = position.coords.latitude, // 위도
 					lon = position.coords.longitude; // 경도
 
-				var locPosition = new kakao.maps.LatLng(lat, lon);
+				let locPosition = new kakao.maps.LatLng(lat, lon);
 
 				// 마커와 인포윈도우를 표시합니다
 				displayMarker(locPosition);
@@ -50,8 +51,7 @@ const AddToilet = () => {
 		} else {
 			// HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
 
-			var locPosition = new kakao.maps.LatLng(33.450701, 126.570667);
-
+			const locPosition = new kakao.maps.LatLng(33.450701, 126.570667);
 			displayMarker(locPosition);
 		}
 
@@ -60,10 +60,47 @@ const AddToilet = () => {
 			marker = new kakao.maps.Marker({
 				map: map,
 				position: locPosition,
+				image: markerImage,
 			});
 
 			// 지도 중심좌표를 접속위치로 변경합니다
 			map.setCenter(locPosition);
+		}
+
+		const geocoder = new kakao.maps.services.Geocoder();
+
+		// 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
+		searchDetailAddrFromCoords(map.getCenter(), displayCenterInfo);
+
+		// 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록합니다
+		kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
+			searchDetailAddrFromCoords(mouseEvent.latLng, function (result, status) {
+				if (status === kakao.maps.services.Status.OK) {
+					let infoDiv = document.querySelector('.infoText');
+					infoDiv.innerHTML = result[0].address.address_name;
+
+					marker.setPosition(mouseEvent.latLng);
+					marker.setMap(map);
+				}
+			});
+		});
+
+		// 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
+		kakao.maps.event.addListener(map, 'idle', function () {
+			searchDetailAddrFromCoords(map.getCenter(), displayCenterInfo);
+		});
+
+		function searchDetailAddrFromCoords(coords, callback) {
+			// 좌표로 법정동 상세 주소 정보를 요청합니다
+			geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+		}
+
+		// 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
+		function displayCenterInfo(result, status) {
+			if (status === kakao.maps.services.Status.OK) {
+				let infoDiv = document.querySelector('.infoText');
+				infoDiv.innerHTML = result[0].address.address_name;
+			}
 		}
 	}, []);
 
@@ -77,7 +114,9 @@ const AddToilet = () => {
 					<div className={styles.checkAddress}>
 						<p>이 주소가 맞나요?</p>
 						<div className={styles.btn}>
-							<p className="infoText"></p>
+							<div className={styles.address}>
+								<p className="infoText"></p>
+							</div>
 							<BlueBtn text="이 위치로 주소 설정" />
 						</div>
 					</div>
