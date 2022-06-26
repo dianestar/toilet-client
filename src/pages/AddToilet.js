79d340dd10493e9b800442319437e10981c3850f
Jetom88 <shinhro3o@gmail.com>
2022-06-26 20:22:50 +0900

    🛠 Fix: Take current position marker and fix nav bar css

--- src/pages/AddToilet.js
+++ src/pages/AddToilet.js
@@ -6,7 +6,6 @@ import AskReview from '../components/modal/AskReview';
 import styles from '../styles/pages/addToilet.module.scss';
 import BlueBtn from '../components/common/BlueBtn';
 import pinSelectedNoBg from '../assets/icons/pinSelectedNoBg.svg';
-import classNames from 'classnames';
 
 const AddToilet = () => {
 	const [open, setOpen] = useState(true);
@@ -14,7 +13,7 @@ const AddToilet = () => {
 	useEffect(() => {
 		const mapContainer = document.getElementById('map'), // 지도를 표시할 div
 			mapOption = {
-				center: new kakao.maps.LatLng(37.54699, 127.09598), // 지도의 중심좌표
+				center: new kakao.maps.LatLng(37.56646, 126.98121), // 지도의 중심좌표
 				level: 3, // 지도의 확대 레벨
 			};
 
@@ -39,19 +38,26 @@ const AddToilet = () => {
 
 		if (navigator.geolocation) {
 			// GeoLocation을 이용해서 접속 위치를 얻어옵니다
-			navigator.geolocation.getCurrentPosition(function (position) {
-				let lat = position.coords.latitude, // 위도
-					lon = position.coords.longitude; // 경도
+			navigator.geolocation.getCurrentPosition(
+				function (position) {
+					let lat = position.coords.latitude, // 위도
+						lon = position.coords.longitude; // 경도
 
-				let locPosition = new kakao.maps.LatLng(lat, lon);
+					let locPosition = new kakao.maps.LatLng(lat, lon);
 
-				// 마커와 인포윈도우를 표시합니다
-				displayMarker(locPosition);
-			});
+					// 마커와 인포윈도우를 표시합니다
+					displayMarker(locPosition);
+				},
+				function (err) {
+					if (err) {
+						console.warn(`ERROR(${err.code}): ${err.message}`);
+					}
+				},
+			);
 		} else {
 			// HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
 
-			const locPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+			const locPosition = new kakao.maps.LatLng(37.56646, 126.98121);
 			displayMarker(locPosition);
 		}
 
@@ -65,19 +71,19 @@ const AddToilet = () => {
 
 			// 지도 중심좌표를 접속위치로 변경합니다
 			map.setCenter(locPosition);
+			searchDetailAddrFromCoords(map.getCenter(), displayCenterInfo);
 		}
 
 		const geocoder = new kakao.maps.services.Geocoder();
 
 		// 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
-		searchDetailAddrFromCoords(map.getCenter(), displayCenterInfo);
 
 		// 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록합니다
 		kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
 			searchDetailAddrFromCoords(mouseEvent.latLng, function (result, status) {
 				if (status === kakao.maps.services.Status.OK) {
-					let infoDiv = document.querySelector('.infoText');
-					infoDiv.innerHTML = result[0].address.address_name;
+					let infoText = document.querySelector('.infoText');
+					infoText.innerHTML = result[0].address.address_name;
 
 					marker.setPosition(mouseEvent.latLng);
 					marker.setMap(map);
@@ -86,9 +92,6 @@ const AddToilet = () => {
 		});
 
 		// 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
-		kakao.maps.event.addListener(map, 'idle', function () {
-			searchDetailAddrFromCoords(map.getCenter(), displayCenterInfo);
-		});
 
 		function searchDetailAddrFromCoords(coords, callback) {
 			// 좌표로 법정동 상세 주소 정보를 요청합니다
@@ -98,8 +101,8 @@ const AddToilet = () => {
 		// 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
 		function displayCenterInfo(result, status) {
 			if (status === kakao.maps.services.Status.OK) {
-				let infoDiv = document.querySelector('.infoText');
-				infoDiv.innerHTML = result[0].address.address_name;
+				let infoText = document.querySelector('.infoText');
+				infoText.innerHTML = result[0].address.address_name;
 			}
 		}
 	}, []);
