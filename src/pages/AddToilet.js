21ae76996d26334e640dea4fc161b0cf66810a90
Jetom88 <shinhro3o@gmail.com>
2022-06-30 21:31:28 +0900

    feat: Editing addtoilet page

--- src/pages/AddToilet.js
+++ src/pages/AddToilet.js
@@ -20,14 +20,9 @@ const AddToilet = () => {
 		const map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
 		const imageSrc = pinSelectedNoBg, // 마커이미지의 주소입니다
-			imageSize = new kakao.maps.Size(22, 32), // 마커이미지의 크기입니다
-			imageOption = { offset: new kakao.maps.Point(22, 32) };
+			imageSize = new kakao.maps.Size(22, 32); // 마커이미지의 크기입니다
 
-		const markerImage = new kakao.maps.MarkerImage(
-			imageSrc,
-			imageSize,
-			imageOption,
-		);
+		const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
 		let marker = new kakao.maps.Marker({
 			// 지도 중심좌표에 마커를 생성합니다
