a2684825950262e2fc282c058a8a45fabcfc9c6d
Jetom88 <shinhro3o@gmail.com>
2022-07-13 20:17:57 +0900

    💄Design: Change login ui and change main color

--- src/pages/common/Login.js
+++ src/pages/common/Login.js
@@ -1,32 +1,100 @@
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { useForm, FormProvider } from 'react-hook-form';
 import BlueBtn from '../../components/common/BlueBtn';
 import Btn from '../../components/common/Btn';
 import Header from '../../components/common/Header';
 import Layout from '../../components/common/Layout';
 import styles from '../../styles/pages/login.module.scss';
+import { POST_LOGIN } from '../../core/_axios/login';
+import { profile } from '../../core/_reducers/profileInfo';
+import EmailInput from '../../components/common/EmailInput';
+import PasswordInput from '../../components/common/PasswordInput';
 
 const Login = () => {
+	const methods = useForm();
 	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
-	const goToLogin = () => {
-		navigate('/login');
+	const onSubmit = async () => {
+		const form = {
+			email: methods.watch('email'),
+			password: methods.watch('pw'),
+		};
+		try {
+			const {
+				data: { success, data, message },
+			} = await POST_LOGIN(form);
+
+			if (success) {
+				localStorage.setItem('token', data.token);
+				navigate('/map');
+
+				dispatch(
+					profile({
+						nickname: data.nickname,
+						imgUrl: data.imgUrl,
+						email: data.email,
+					}),
+				);
+			} else {
+				alert(message);
+			}
+		} catch (e) {
+			console.log(e);
+		}
 	};
 
 	return (
 		<Layout>
 			<Header type="none" text="로그인" />
 			<section className={styles.loginSection}>
-				<div className={styles.logo}></div>
-				<Btn text="카카오톡으로 시작하기" color={true} />
-				<BlueBtn text="이메일로 시작하기" />
-				<p
-					className={styles.loginBtn}
-					onClick={() => {
-						goToLogin();
-					}}
-				>
-					이미 계정이 있으세요? <span>로그인</span>
-				</p>
+				<div className={styles.logo}>
+					<img
+						src="./images/KakaoTalk_Photo_2022-07-12-17-55-23.jpeg"
+						alt="logo"
+					/>
+				</div>
+				<div className={styles.login}>
+					<FormProvider {...methods}>
+						<form
+							className={styles.form}
+							onSubmit={methods.handleSubmit(onSubmit)}
+						>
+							<EmailInput />
+							<PasswordInput withCheck={false} text="비밀번호" />
+							<div className={styles.signIn}>
+								<BlueBtn text="로그인" />
+							</div>
+						</form>
+					</FormProvider>
+					<p className={styles.info}>
+						<span
+							onClick={() => {
+								navigate('/register_account');
+							}}
+						>
+							회원가입
+						</span>{' '}
+						|{' '}
+						<span
+							onClick={() => {
+								navigate('/findpwA');
+							}}
+						>
+							비밀번호 찾기
+						</span>
+					</p>
+
+					<div className={styles.line} />
+
+					<div className={styles.signUp}>
+						<button className={styles.kakao}>
+							<img src="./images/kakaoLogo.png" className={styles.kakaoLogo} />
+							<p>로 회원가입</p>
+						</button>
+					</div>
+				</div>
 			</section>
 		</Layout>
 	);
